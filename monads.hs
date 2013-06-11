2d6027d1afb74e506e405b7ac97a6733e4700428
Frank-Siebert <github.com@genialitaet.de>
2013-06-11 17:44:16 +0200

    Add a simple (and possibly imcomplete) monad to interject IOs

--- monads.hs
+++ monads.hs
@@ -33,4 +33,21 @@ instance Monad Intree where
   (Node left x right) >>= f = case f x of
       Nil -> Nil -- what about left and right?
       (Node left' y right') -> Node (left >>= f) y right'  -- todo! what about left' and right?
+      
+data IIO a = IIO (IO a) a
+instance Monad (IIO) where
+  return x = IIO (return x) x
+  (IIO io x) >>= f = let (IIO io' x') = f x in IIO (io >> putStrLn "interject"  >> io') x'
+--(IIO io x) >>= f = let (IIO io' x') = f x in IIO (io >>= (\tmp -> putStrLn "interject" >> return tmp ) >>= io') x'
 
+runIIO (IIO io x) = io
+r :: IO () -> IIO ()
+r io = IIO io ()
+
+--rf :: a -> (a -> IO b) -> IIO b
+--rf x f = IIO (a >> 
+
+
+-- works:
+-- runIIO $ IIO (putStrLn "1") () >> r (putStrLn "2") >> r (putStrLn "3")
+-- runIIO $ IIO (putStrLn "1") () >> return "Hallo" >>= r . putStrLn 
