f88eb19a381748e2bad3245e265cb6fca4305664
Frank-Siebert <github.com@genialitaet.de>
2013-11-09 00:00:26 +0100

    Change CircList to have at least one element
    - allows to make CircList an instance of Comonad

--- CircList.hs
+++ CircList.hs
@@ -9,13 +9,18 @@ module Data.CircList
   ) where
 
 --
-data CircList a = CircL ([a] -> [a]) [a]
+data CircList a = CircL ([a] -> [a]) a [a]
 
-normalize :: CircList a -> CircList a
-normalize (CircList f []) = CircList id (f [])
+-- difflists cannot be functors!
+--instance Functor CircList where
+--  fmap func (CircL f hd xs) = CircL (map func . f) (func hd) (map func xs)
 
-fromList :: [a] -> CircList a
-fromList xs = CircL id xs
+mkCircList :: a -> [a] -> CircList a
+mkCircList x xs = CircL id x xs
 
-head :: CircList a
-head (CircList _ (x:_)) = x
+head :: CircList a -> a
+head (CircL _ x _) = x
+
+pop :: CircList a -> CircList a
+pop (CircL f hd (x:xs)) = CircL f x xs
+
