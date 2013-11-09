4bfbb4182328e7622e446b945653632ef267d17c
Frank-Siebert <github.com@genialitaet.de>
2013-11-09 22:16:31 +0100

    CircList: rename "advance" to "right", add left

--- CircList.hs
+++ CircList.hs
@@ -4,20 +4,26 @@ module Data.CircList
   , toFiniteList
   , toInfList
   , rewind
-  , advance
+  , left
+  , right
   , replace
   , isWrap
   ) where
   
 import Prelude hiding (head)
+import Control.Comonad
 
 --
 data CircList a = C [a] a [a]
 -- internally normalized: rs==[] implies ls==[]
+-- this normalization is bullshit. It is fine if there is no central element.
 
 -- difflists cannot be functors!
 instance Functor CircList where
   fmap f (C ls h rs) = C (map f ls) (f h) (map f rs)
+  
+instance (Show a) => Show (CircList a) where
+  show (C ls h rs) = show (reverse ls) ++ show h ++ show rs
 
 mkCircList :: a -> [a] -> CircList a
 mkCircList x xs = C [] x xs
@@ -31,11 +37,15 @@ toFiniteList (C ls h rs) = (h:rs) ++ reverse ls
 toInfList :: CircList a -> [a]
 toInfList = cycle . toFiniteList
 
-advance :: CircList a -> CircList a
-advance cl@(C [] _ []) = cl
-advance    (C ls h [r]) = C [] r (reverse (h:ls))
-advance    (C ls h (r:rs)) = C (h:ls) r rs
-advance    _ = error "CircList is not normalized"
+left :: CircList a -> CircList a
+left cl@(C [] _ []) = cl
+left    (C [] h rs) = let (x:xs) = reverse (h:rs) in C (xs) x []
+left    (C (l:ls) h rs) = C ls l (h:rs)
+
+right :: CircList a -> CircList a
+right cl@(C [] _ []) = cl
+right    (C ls h []) = let (x:xs) = reverse (h:ls) in C [] x xs 
+right    (C ls h (r:rs)) = C (h:ls) r rs
 
 replace :: a -> CircList a -> CircList a
 replace x (C ls _ rs) = C ls x rs
