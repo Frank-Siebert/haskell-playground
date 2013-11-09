6dc4e0e49b2eb6df3d68243fbb6669168b061921
Frank-Siebert <github.com@genialitaet.de>
2013-11-09 11:13:33 +0100

    Implement most functions of CircList

--- CircList.hs
+++ CircList.hs
@@ -1,26 +1,49 @@
 module Data.CircList
-  ( fromList
-  , toList
+  ( mkCircList
   , head
-  , take
+  , toFiniteList
+  , toInfList
+  , rewind
   , advance
   , replace
   , isWrap
   ) where
+  
+import Prelude hiding (head)
 
 --
-data CircList a = CircL ([a] -> [a]) a [a]
+data CircList a = C [a] a [a]
+-- internally normalized: rs==[] implies ls==[]
 
 -- difflists cannot be functors!
---instance Functor CircList where
---  fmap func (CircL f hd xs) = CircL (map func . f) (func hd) (map func xs)
+instance Functor CircList where
+  fmap f (C ls h rs) = C (map f ls) (f h) (map f rs)
 
 mkCircList :: a -> [a] -> CircList a
-mkCircList x xs = CircL id x xs
+mkCircList x xs = C [] x xs
 
 head :: CircList a -> a
-head (CircL _ x _) = x
+head (C _ x _) = x
 
-pop :: CircList a -> CircList a
-pop (CircL f hd (x:xs)) = CircL f x xs
+toFiniteList :: CircList a -> [a]
+toFiniteList (C ls h rs) = (h:rs) ++ reverse ls
 
+toInfList :: CircList a -> [a]
+toInfList = cycle . toFiniteList
+
+advance :: CircList a -> CircList a
+advance cl@(C [] _ []) = cl
+advance    (C ls h [r]) = C [] r (reverse (h:ls))
+advance    (C ls h (r:rs)) = C (h:ls) r rs
+advance    _ = error "CircList is not normalized"
+
+replace :: a -> CircList a -> CircList a
+replace x (C ls _ rs) = C ls x rs
+
+rewind :: CircList a -> CircList a
+rewind cl@(C [] _ _) = cl
+rewind (C (l:ls) h rs) = rewind (C ls l (h:rs))
+
+isWrap :: CircList a -> Bool
+isWrap (C [] _ _) = True
+isWrap _ = False
