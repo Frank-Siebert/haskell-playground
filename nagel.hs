e1033916d700410dad7b88f1d5c9a0108889f3fa
Frank-Siebert <github.com@genialitaet.de>
2013-11-14 17:06:59 +0100

    Add list of randoms to Car.

--- nagel.hs
+++ nagel.hs
@@ -4,15 +4,17 @@ import Data.CircList
 import Data.List (intercalate)
 import Control.Comonad
 
-data Cell = Empty | Car Int deriving (Eq)
+data Cell = Empty | Car { speed :: Int 
+                        , random :: [Int] }
+  deriving (Eq)
 
 instance Show Cell where
     show Empty = "."
-    show (Car n) = show n
+    show (Car n _) = show n
 
 toChar :: Cell -> Char    
 toChar Empty = '.'
-toChar (Car n) = Prelude.head . show $ n
+toChar (Car n _) = Prelude.head . show $ n
 
 count :: (Eq a) => a -> [a] -> Int
 count x = length . takeWhile (x==)
@@ -31,7 +33,7 @@ cl (x:xs) = mkCircList [] x xs
 makeRoad :: Int -> Road
 makeRoad len = cl (makeRoad' $ take len infiniteRandomList) where
            makeRoad' = map (\n -> if (n `mod` (maxSpeed - 1)) == 0 
-                                then Car (n `mod` maxSpeed)
+                                then Car (n `mod` maxSpeed) (randoms . mkStdGen $ n)
                                 else Empty)
 
 -- debug / test:
@@ -40,13 +42,13 @@ rd = makeRoad 10
 
 updateSpeed :: [Cell] -> Cell
 updateSpeed (Empty:_) = Empty
-updateSpeed (Car curSpeed:ahead) = let freeRoad = count Empty ahead
-                                       newSpeed = minimum [curSpeed + 1, freeRoad, maxSpeed]
-                                    in Car newSpeed
+updateSpeed (Car curSpeed rnd:ahead) = let freeRoad = count Empty ahead
+                                           newSpeed = minimum [curSpeed + 1, freeRoad, maxSpeed]
+                                        in Car newSpeed rnd
 
 move :: [Cell] -> Cell
 move = move' 0 where
-       move' n (c@(Car m):_) | n==m = c
+       move' n (c@(Car m _):_) | n==m = c
        move' n (_:xs)     | n>maxSpeed = Empty
                           | otherwise = move' (n+1) xs
 
