f3f4699be282143b3d25a88e25fbe8a9fc6071c9
Frank-Siebert <github.com@genialitaet.de>
2014-07-12 11:50:43 +0200

    Add followups
    - miraculously is correct!

--- Solitaire/src/main.hs
+++ Solitaire/src/main.hs
@@ -3,7 +3,11 @@ import Solitaire.Solitaire
 import Solitaire.Display
 
 
+followups :: [GameState]
+followups = let (GameState t ud) = gs in [(GameState t' ud) | t' <- moves t]
+
+showFollowups = concatMap (\x -> tag "h2" "Followup" ++ displayGameState x) followups
 
 main :: IO ()
 main = do
-    putStr $ displayHtml (displayGameState gs)
+    putStr $ displayHtml $ (displayGameState gs) ++showFollowups
