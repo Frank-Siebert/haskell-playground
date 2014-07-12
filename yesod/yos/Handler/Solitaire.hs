eabef36f8b77e4038ea640d3bc9412ed10fe5ae2
Frank-Siebert <github.com@genialitaet.de>
2014-07-12 08:27:41 +0200

    Add a new yesod project again
    - it finally works!

---
+++ yesod/yos/Handler/Solitaire.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE TupleSections, OverloadedStrings #-}
+module Handler.Solitaire where
+
+import Import
+
+-- This is a handler function for the GET request method on the HomeR
+-- resource pattern. All of your resource patterns are defined in
+-- config/routes
+--
+-- The majority of the code you will write in Yesod lives in these handler
+-- functions. You can spread them across multiple files if you are so
+-- inclined, or create a single monolithic file.
+getSolitaireR :: Handler Html
+getSolitaireR = do
+    (formWidget, formEnctype) <- generateFormPost sampleForm
+    let submission = Nothing :: Maybe (FileInfo, Text)
+        handlerName = "getHomeR" :: Text
+    defaultLayout $ do
+        aDomId <- newIdent
+        setTitle "Solitaire display"
+        $(widgetFile "homepage")
+
+sampleForm :: Form (FileInfo, Text)
+sampleForm = renderDivs $ (,)
+    <$> fileAFormReq "Choose a file"
+    <*> areq textField "What's on the file?" Nothing
