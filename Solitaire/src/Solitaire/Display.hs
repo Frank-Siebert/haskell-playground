c500bceacad0ce216764fd92ad4066898929b112
Frank-Siebert <github.com@genialitaet.de>
2014-07-12 10:20:58 +0200

    Align columns at top

--- Solitaire/src/Solitaire/Display.hs
+++ Solitaire/src/Solitaire/Display.hs
@@ -4,7 +4,8 @@ import Solitaire.Data
 
 displayHtml :: GameState -> String
 displayHtml gs = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">" ++
-                 tag "html" ( tag "head" "<style type=\"text/css\"><!-- .card { //border: 2px solid black; height: 75px; max-height: 75px; overflow:hidden;}--></style> ")
+                 tag "html" ( tag "head" "<style type=\"text/css\"><!-- .card { //border: 2px solid black; height: 75px; max-height: 75px; overflow:hidden;}\n\
+                 \td { vertical-align:top} --></style> ")
                  ++ tag "body" (displayGameState gs)
 
 displayGameState :: GameState -> String
