31a8f4ed920185553889ad77f38f7c9cb38d898f
Frank-Siebert <github.com@genialitaet.de>
2014-07-12 12:23:21 +0200

    Do more than one move

--- Solitaire/src/main.hs
+++ Solitaire/src/main.hs
@@ -1,12 +1,19 @@
 import Solitaire.Data
 import Solitaire.Solitaire
 import Solitaire.Display
+import Control.Monad((>=>))
 
 
 followups :: [GameState]
-followups = let (GameState t ud) = gs in [(GameState t' ud) | t' <- moves t]
+followups = let (GameState t ud) = gs in [(GameState t' ud) | t' <- iterateM moves 2 t]
 
+showFollowups :: String
 showFollowups = concatMap (\x -> tag "h2" "Followup" ++ displayGameState x) followups
+
+iterateM :: (Monad m) => (a -> m a) -> Int -> a -> m a
+iterateM k 0 = return
+iterateM k 1 = k
+iterateM k n = k >=> iterateM k (n-1)
 
 main :: IO ()
 main = do
