c6f89a97c139c0803a69310540e1b5aeda3ae95a
Frank-Siebert <github.com@genialitaet.de>
2017-12-21 19:22:44 +0100

    Add Unipair (value with previous and current)

---
+++ Unipair.hs
@@ -0,0 +1,19 @@
+data Unipair a = Unipair a a
+type NowThen = Unipair
+
+instance Functor Unipair where
+   fmap f (Unipair x y) = Unipair (f x) (f y)
+instance Applicative Unipair where
+   pure x = Unipair x x
+   Unipair f f' <*> Unipair x x' = Unipair (f x) (f' x')
+instance Monad Unipair where
+   return = pure
+   Unipair x x' >>= f = let Unipair y _ = f x
+                            Unipair _ z = f x'
+                         in Unipair y z
+
+instance Foldable Unipair where
+    foldMap f (Unipair x y) = f x `mappend` f y
+
+instance Traversable Unipair where
+    traverse f (Unipair x y) = Unipair <$> f x <*> f y
