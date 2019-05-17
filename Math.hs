5a69947f5a7a6bc5483d03b216893000e7a47329
Frank-Siebert <github.com@genialitaet.de>
2019-05-17 18:22:36 +0200

    Just have Terms without special arities and special operations

--- Math.hs
+++ Math.hs
@@ -1,39 +1,38 @@
 {-# LANGUAGE FlexibleInstances #-}
 
-data Term uo bo mo n v = Lit n | Var v
-              | UnaryTerm uo (Term uo bo mo n v)
-              | BinaryTerm bo (Term uo bo mo n v) (Term uo bo mo n v)
-              | MultiTerm mo [Term uo bo mo n v] deriving Show
+data Term op n v = Lit n | Var v
+              | Terms op [Term op n v] deriving Show
 
--- data NamedOp = SumOp | DiffOp | FactorOp | QuotientOp | ExpOp | NegOp
-data UnaryOperations = UnNegate | UnAbs | UnSignum deriving (Eq,Show)
-data MultiOperations = MoSum | MoProduct deriving (Eq,Show)
-data BinaryOperations = BoDifference | BoQuotient deriving (Eq,Show)
+data Operation  = UnNegate | UnAbs | UnSignum
+                | MoSum | MoProduct
+                | BoDifference | BoQuotient deriving (Eq,Show)
 
-type TermStdOp = Term UnaryOperations BinaryOperations MultiOperations
+isAssoc :: Operation -> Bool
+isAssoc MoSum = True
+isAssoc MoProduct = True
+isAssoc _ = False
 
-instance Num n => Num (Term UnaryOperations BinaryOperations MultiOperations n v) where
+type TermStdOp = Term Operation
+
+instance Num n => Num (Term Operation n v) where
    fromInteger n = Lit (fromInteger n)
-   negate n = UnaryTerm UnNegate n
-   abs n = UnaryTerm UnAbs n
-   signum n = UnaryTerm UnSignum n
-   a + b = MultiTerm MoSum [a,b]
-   a - b = BinaryTerm BoDifference a b
-   a * b = MultiTerm MoProduct [a,b]
+   negate n = Terms UnNegate [n]
+   abs n = Terms UnAbs [n]
+   signum n = Terms UnSignum [n]
+   a + b = Terms MoSum [a,b]
+   a - b = Terms BoDifference [a,b]
+   a * b = Terms MoProduct [a,b]
 
 x :: TermStdOp n String
 x = Var "x"
 
-collectAssoc :: (Eq mo) => Term uo bo mo n v -> Term uo bo mo n v
-collectAssoc (MultiTerm op terms) = MultiTerm op (concat (map embed terms)) where
-   embed (MultiTerm op' terms') | op == op' = concat (map embed terms')
-                                | otherwise = [collectAssoc (MultiTerm op' (map collectAssoc terms'))]
-   embed (BinaryTerm op' t1 t2 ) = [BinaryTerm op' (collectAssoc t1) (collectAssoc t2)]
-   embed (UnaryTerm op' term) = [UnaryTerm op' (collectAssoc term)]
+collectAssoc :: (Eq op) => (op -> Bool) -> Term op n v -> Term op n v
+collectAssoc assoc (Terms op terms) | assoc op = Terms op (concat (map embed terms))
+                              | otherwise  = Terms op (map (collectAssoc assoc) terms) where
+   embed (Terms op' terms') | op == op' = concat (map embed terms')
+                            | otherwise = [collectAssoc assoc (Terms op' (map (collectAssoc assoc) terms'))]
    embed other = [other]
-collectAssoc (UnaryTerm op term) = UnaryTerm op (collectAssoc term)
-collectAssoc (BinaryTerm op t1 t2) = BinaryTerm op (collectAssoc t1) (collectAssoc t2)
-collectAssoc other = other
+collectAssoc _ other = other
 
-takeVars :: Term ou bo mo n v -> [v]
+takeVars :: Term op n v -> [v]
 takeVars (Var v) = [v]
